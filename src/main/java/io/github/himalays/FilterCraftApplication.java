934a80ec931f297e240888b8ae3542a176d0ca80
sujit-sharma <sujit.sharma@8squarei.com>
2024-12-06 16:47:36 +0545

    Initial setup

---
+++ src/main/java/io/github/himalays/FilterCraftApplication.java
@@ -0,0 +1,13 @@
+package io.github.himalays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterCraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilterCraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/himalays/FilterCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.himalays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterCraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
